424436382ffba07310b752f83cbee2dd9db8b85b
hewrin <hewrin@hotmail.com>
2015-11-26 23:42:14 +0800

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationships",
+																	foreign_key: "follower_id",
+																	dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+
+	has_many :passive_relationships, class_name: "Relationships",
+																	 foreign_key: "followed_id",
+																	 dependent: :destroy
+	has_many :followers, through: :passive_relationships, source: :follower
 	attr_accessor :remember_token, :activation_token, :reset_token
 	has_secure_password
 	before_save { self.email = email.downcase }
@@ -59,6 +68,23 @@ class User < ActiveRecord::Base
 
 	def password_reset_expired?
 		reset_sent_at < 2.hours.ago
+	end
+
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def following?(other_user)
+		following.include?(other_user)
+	end
+
+	def feed
+	    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+	    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -18,13 +18,19 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :account_activation, only: [:edit]
 
   resources :password_resets, only: [:edit, :new, :create, :update]
 
   resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
 
   get 'login' => 'sessions#new'
 
