e1c8c5b4348ee1ff646f6d276fac0bb60604621f
Faris <hewrin@hotmail.com>
2016-08-25 10:23:34 +0800

    add new method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,10 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 6}, allow_nil: true
 
+	def introduce
+		"Hi my name is #{name}"
+	end
+
 	#Returns the hash digest of the given string
 	def self.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
